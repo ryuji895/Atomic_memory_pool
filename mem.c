8e227ba0087277995ba02a15d7c21fe13db63639
りゅうじ <ryuji4263@gmail.com>
2025-07-30 01:11:10 +0900

    Update mem.c

--- mem.c
+++ mem.c
@@ -8,6 +8,7 @@
 //仮のuser_info_t
 typedef struct {
 	int sock;
+	uint8_t flag[2];
 	char user_uuid[36];
 	char user_email[255];//RFC5321、まぁ、今回は関係ないけど
 }user_info_t;
@@ -39,56 +40,54 @@ typedef struct {
 
 memory_pool_t user_pool;
 
-flag_t allocate_memory(memory_pool_t *pool);//メモリを借りる為の関数
-void release_memory(memory_pool_t *pool,flag_t flag);//メモリを返す関数
+user_info_t *allocate_memory(memory_pool_t *pool);//メモリを借りる為の関数
+void release_memory(memory_pool_t *pool,user_info_t *flag);//メモリを返す関数
 void init_memory_pool(memory_pool_t *pool);//ただの0埋め
 
 
-//かのじょほしいー
-flag_t allocate_memory(memory_pool_t *pool){
-	flag_t answer;
+user_info_t *allocate_memory(memory_pool_t *pool){
+	uint8_t flag[2];
 	TaggedValueUnion expected_union;
 	TaggedValueUnion desired_union;
 allocate_memory_up_bit_flag1:
 	expected_union.u128=atomic_load_explicit(&pool->flag1,memory_order_acquire);
 	if(__builtin_expect(expected_union.s.flag==UINT64_MAX,0)){
-		answer.flag[0]=0xff;
-		return answer;
+		return NULL;
 	}
 	desired_union.u128=expected_union.u128;
 	desired_union.s.version++;
-	answer.flag[0]= (uint8_t)__builtin_ctzll(~expected_union.s.flag);
-	desired_union.s.flag|=1ULL<<answer.flag[0];
+	flag[0]= (uint8_t)__builtin_ctzll(~expected_union.s.flag);
+	desired_union.s.flag|=1ULL<<flag[0];
 	if(__builtin_expect(!atomic_compare_exchange_strong_explicit(&pool->flag1,&expected_union.u128,desired_union.u128,memory_order_release,memory_order_acquire),0))goto allocate_memory_up_bit_flag1;
 	//flag2
 allocate_memory_up_bit_flag2:
-	expected_union.u128=atomic_load_explicit(&pool->flag2[answer.flag[0]],memory_order_acquire);
+	expected_union.u128=atomic_load_explicit(&pool->flag2[flag[0]],memory_order_acquire);
 	if(__builtin_expect(expected_union.s.flag==UINT64_MAX,0))goto allocate_memory_up_bit_flag1;
 	desired_union.u128=expected_union.u128;
 	desired_union.s.version++;
-	answer.flag[1]=(uint8_t)__builtin_ctzll(~expected_union.s.flag);
-	desired_union.s.flag|=1ULL<<answer.flag[1];
-	if(__builtin_expect(!atomic_compare_exchange_strong_explicit(&pool->flag2[answer.flag[0]],&expected_union.u128,desired_union.u128,memory_order_release,memory_order_acquire),0))goto allocate_memory_up_bit_flag2;
-	return answer;
+	flag[1]=(uint8_t)__builtin_ctzll(~expected_union.s.flag);
+	desired_union.s.flag|=1ULL<<flag[1];
+	if(__builtin_expect(!atomic_compare_exchange_strong_explicit(&pool->flag2[flag[0]],&expected_union.u128,desired_union.u128,memory_order_release,memory_order_acquire),0))goto allocate_memory_up_bit_flag2;
+	return &user_pool.pool[flag[0]][flag[1]];
 }
 
-void release_memory(memory_pool_t *pool,flag_t flag){
+void release_memory(memory_pool_t *pool,user_info_t *info){
 	TaggedValueUnion expected_union;//ここAIのねーみんぐ
 	TaggedValueUnion desired_union;//ここAIのネーミング
 	//一旦メモリからロード
 release_memory_down_bit_flag_flag2:
-	expected_union.u128=atomic_load_explicit(&pool->flag2[flag.flag[0]],memory_order_acquire);
+	expected_union.u128=atomic_load_explicit(&pool->flag2[info->flag[0]],memory_order_acquire);
 	desired_union.u128=expected_union.u128;//コピー
 	desired_union.s.version++;
-	desired_union.s.flag &=~(1ULL<<flag.flag[1]);
-	if(__builtin_expect(!atomic_compare_exchange_strong_explicit(&pool->flag2[flag.flag[0]],&expected_union.u128,desired_union.u128,memory_order_release,memory_order_acquire),0))goto release_memory_down_bit_flag_flag2;
+	desired_union.s.flag &=~(1ULL<<info->flag[1]);
+	if(__builtin_expect(!atomic_compare_exchange_strong_explicit(&pool->flag2[info->flag[0]],&expected_union.u128,desired_union.u128,memory_order_release,memory_order_acquire),0))goto release_memory_down_bit_flag_flag2;
 	//今度はflag1を設定する。
 	//ifを入れるとヒットしなかったら嫌だから。
 release_memory_down_bit_flag_flag1:
 	expected_union.u128=atomic_load_explicit(&pool->flag1,memory_order_acquire);
 	desired_union.u128=expected_union.u128;
 	desired_union.s.version++;
-	desired_union.s.flag &=~(1ULL<<flag.flag[0]);
+	desired_union.s.flag &=~(1ULL<<info->flag[0]);
 	if(__builtin_expect(!atomic_compare_exchange_strong_explicit(&pool->flag1,&expected_union.u128,desired_union.u128,memory_order_release,memory_order_acquire),0))goto release_memory_down_bit_flag_flag1;
 	return;
 }
